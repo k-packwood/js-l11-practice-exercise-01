99740b497951cbfebdc5962a5229d3e6905a14ca
Kai Packwood <kai.packwood@igd.com>
2021-10-03 15:28:25 +0100

    Retrieves and displays user data from API

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
 const randomFolks = document.querySelector(".random-peeps");
+
+const getData = async function () {
+    const userRequest = await fetch("https://randomuser.me/api?results=5");
+    const data = await userRequest.json();
+    const userResults = data.results;
+
+    displayUsers(userResults);
+};
+
+const displayUsers = function (userResults) {
+    randomFolks.innerHTML = "";
+
+    for (let user of userResults) {
+        let country = user.location.country;
+        let name = user.name.first;
+        let imageUrl = user.picture.medium;
+
+        const userDiv = document.createElement("div");
+        userDiv.innerHTML = `<h3>${name}</h3>
+            <p>${country}</p>
+            <img src=${imageUrl} alt="User avatar" />`;
+
+        randomFolks.append(userDiv);
+    }
+};
+
+getData();
